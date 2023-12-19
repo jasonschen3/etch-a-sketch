7b2101d72d35d7620ee601c6a26a0a8b42998d05
Jason Chen <jasonchen@Jasons-MacBook-Pro.local>
2023-12-18 17:26:34 -0700

    Add UI functionality

--- sketch.js
+++ sketch.js
@@ -1,29 +1,53 @@
+// Initial setup
 const container = document.createElement("div");
 container.classList.add("container");
-
-// Create the boxes
-for (let i = 1; i <= 4; i++) {
-  const currentRow = document.createElement("div");
-  currentRow.classList.add("row");
-  currentRow.id = `row-${i}`;
-  for (let j = 1; j <= 4; j++) {
-    const gridCell = document.createElement("div");
-    gridCell.id = `col-${j}`;
-    gridCell.classList.add("column");
-
-    // Event
-    gridCell.addEventListener("mouseover", () => {
-      changeColor(gridCell, "red");
-    });
-
-    currentRow.appendChild(gridCell);
-  }
-  container.appendChild(currentRow);
-}
-
 document.body.append(container);
+let squaresPerSide = 4;
+createGrid(squaresPerSide);
+
+// Create the boxes, changed to a function to accommodate the requirements
+function createGrid(squaresPerSide) {
+  container.innerHTML = ""; // Clear the existing grid
+
+  for (let i = 1; i <= squaresPerSide; i++) {
+    const currentRow = document.createElement("div");
+    currentRow.classList.add("row");
+    currentRow.id = `row-${i}`;
+    for (let j = 1; j <= squaresPerSide; j++) {
+      const gridCell = document.createElement("div");
+      gridCell.id = `col-${j}`;
+      gridCell.classList.add("column");
+
+      // Event if mouseover then color
+      gridCell.addEventListener("mouseover", () => {
+        changeColor(gridCell, "green");
+      });
+
+      //   // Event if mouse not over, then reset
+      //   gridCell.addEventListener("mouseleave", () => {
+      //     changeColor(gridCell, "white");
+      //   });
+
+      currentRow.appendChild(gridCell);
+    }
+    container.appendChild(currentRow);
+  }
+}
 
 // Function to change color
-function changeColor(element, color = "black") {
+function changeColor(element, color = "white") {
   element.style.backgroundColor = color; // Change to desired color
 }
+
+// Button to change size
+const btn = document.querySelector("#btn");
+btn.addEventListener("click", () => {
+  let newSPS = prompt("Enter new squares per side");
+  if (isNaN(newSPS) || newSPS <= 0) {
+    // Check if the input is a valid number
+    alert("Please enter a valid number!");
+  } else {
+    squaresPerSide = parseInt(newSPS);
+    createGrid(squaresPerSide);
+  }
+});
